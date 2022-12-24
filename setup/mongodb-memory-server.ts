1630a7f5ed3e18c313ea98a46dcd4807eb2ce74a
harryli0088 <harryli0088@gmail.com>
2022-12-24 13:03:26 -0500

    when in doubt, @ts-ignore

--- setup/mongodb-memory-server.ts
+++ setup/mongodb-memory-server.ts
@@ -4,14 +4,18 @@ import { MongoMemoryServer } from 'mongodb-memory-server';
 //inspired by https://github.com/ec965/vitest-mongodb/blob/main/vitest-mongodb/src/index.ts
 
 export async function setup() {
+  //@ts-ignore
   global.__MONGO_DB__ = await MongoMemoryServer.create();
+  //@ts-ignore
   process.env.MONGODB_URI= global.__MONGO_DB__.getUri();
   console.log("Mongodb memory server URI:", process.env.MONGODB_URI);
 }
 
 export async function teardown() {
+  //@ts-ignore
   if (globalThis.__MONGO_DB__) {
     console.log("Stopping mongodb memory server");
+    //@ts-ignore
     await global.__MONGO_DB__.stop();
   }
-}
+}d
